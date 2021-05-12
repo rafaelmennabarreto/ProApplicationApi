7cecbedc3277b9a00e5967b8b7286bd9ed890a13
Rafael <rsmennabarreto@gmail.com>
2021-05-12 12:04:49 -0300

    feat : Call calculateEligibilityScoreUseCase on the controller and add mock for projects

---
+++ src/mock/Projects.ts
@@ -0,0 +1,8 @@
+import Project from "@src/entities/Project";
+
+export const Projects = [
+  new Project("calculate_dark_matter_nasa", 10),
+  new Project("determine_schrodinger_cat_is_alive", 5),
+  new Project("support_users_from_xyz", 3),
+  new Project("collect_information_for_xpto", 2),
+];
--- src/useCases/calculateEligibilityScore/CalculateEligibilityScoreController.ts
+++ src/useCases/calculateEligibilityScore/CalculateEligibilityScoreController.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from "express";
+import CalculateEligibilityScoreUseCase from "./CalculateEligibilityScoreUseCase";
 import ICalculateEligibilityScoreDto from "./dtos/ICalculateEligibilityScoreDto";
 import CalculateEligibilityScoreDtoValidator from "./validators/CalculateEligibilityScoreDtoValidator";
 
 export default class CalculateEligibilityScoreController {
+  constructor(
+    private _CalculateEligibilityUseCase: CalculateEligibilityScoreUseCase
+  ) {}
+
   Handle(request: Request, response: Response) {
-    // create a instance from use case here
     try {
       const data: ICalculateEligibilityScoreDto = request.body;
 
       new CalculateEligibilityScoreDtoValidator(data).validate();
 
-      response.send("tudo certo");
+      const responseData = this._CalculateEligibilityUseCase.Execute(data);
+
+      response.status(200).send(responseData);
     } catch (e) {
       response.status(400).send(e.message);
     }
