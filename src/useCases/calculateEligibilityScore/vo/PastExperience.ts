a7037d8486ffd1686a1374bb58892c0831266311
Rafael <rsmennabarreto@gmail.com>
2021-05-12 12:40:35 -0300

    fix : Adjust Past experience score values

--- src/useCases/calculateEligibilityScore/vo/PastExperience.ts
+++ src/useCases/calculateEligibilityScore/vo/PastExperience.ts
@@ -1,19 +1,24 @@
 export default class PastExperience {
   private _sales: boolean;
   private _support: boolean;
+  private _salesScore: number;
+  private _supportScore: number;
 
   constructor(sales: boolean, support: boolean) {
     this._sales = sales;
     this._support = support;
+    this._salesScore = 5;
+    this._supportScore = 3;
   }
 
   get score() {
-    if (!this._sales && !this._support) return 0;
+    if (this._sales && this._support)
+      return this._salesScore + this._supportScore;
 
-    if (this._sales && this._support) {
-      return 5;
-    }
+    if (this._sales) return this._salesScore;
 
-    return 3;
+    if (this._support) return this._supportScore;
+
+    return 0;
   }
 }
