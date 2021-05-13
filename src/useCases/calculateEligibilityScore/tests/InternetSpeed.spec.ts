d11825313b32a685daa024e35c75a364765415df
Rafael Barreto <rsmennabarreto@gmail.com>
2021-05-13 20:05:52 -0300

    test: Add test for the internet speed

---
+++ src/useCases/calculateEligibilityScore/tests/InternetSpeed.spec.ts
@@ -0,0 +1,26 @@
+import InternetSpeed from "../vo/InternetSpeed";
+
+test("It should be return 0", () => {
+  const internetSpeed = new InternetSpeed(40, 35);
+  expect(internetSpeed.score).toEqual(0);
+});
+
+test("it should be return 1", () => {
+  const internetSpeed = new InternetSpeed(52, 40);
+  expect(internetSpeed.score).toEqual(1);
+});
+
+test("it should be return 2", () => {
+  const internetSpeed = new InternetSpeed(80, 55);
+  expect(internetSpeed.score).toEqual(2);
+});
+
+test("it should be return -1", () => {
+  const internetSpeed = new InternetSpeed(40, 4);
+  expect(internetSpeed.score).toEqual(-1);
+});
+
+test("it should be return -2", () => {
+  const internetSpeed = new InternetSpeed(4, 3);
+  expect(internetSpeed.score).toEqual(-2);
+});
