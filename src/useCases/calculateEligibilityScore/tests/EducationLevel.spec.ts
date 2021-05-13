af100ccfb8f6867aed60e805ef21d66789bcd7ce
Rafael Barreto <rsmennabarreto@gmail.com>
2021-05-13 20:05:26 -0300

    test: Add test for the EducationLevel

---
+++ src/useCases/calculateEligibilityScore/tests/EducationLevel.spec.ts
@@ -0,0 +1,16 @@
+import EducationLevel from "../vo/EducationLevel";
+
+test("It should return 0", () => {
+  const educationLevel = new EducationLevel("no_education");
+  expect(educationLevel.score).toEqual(0);
+});
+
+test("It should return 1", () => {
+  const educationLevel = new EducationLevel("high_school");
+  expect(educationLevel.score).toEqual(1);
+});
+
+test("It should return 2", () => {
+  const educationLevel = new EducationLevel("bachelors_degree_or_high");
+  expect(educationLevel.score).toEqual(2);
+});
